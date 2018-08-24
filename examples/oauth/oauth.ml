581198fd27dcf260e156fab08d76b511d25598e6
Keigo Imai <keigo.imai@gmail.com>
2018-08-24 15:33:31 +0900

    refactoring

--- examples/oauth/oauth.ml
+++ examples/oauth/oauth.ml
@@ -2,52 +2,48 @@
  * OAuth
  *)
 open Mpst.ThreeParty
-open Mpst_http
-module O = Oauth_http
+module H = Mpst_http
+let (>>=) = Lwt.(>>=)
 
 (*
   $ ssh ben -R127.0.0.1:8080:127.0.0.1:8080
   $ dune build --profile=release && _build/default/ocaml-mpst/examples/oauth/oauth.exe
  *)
 
-let (>>=) = Lwt.(>>=)
-
 module Params = struct
   let client_id = "1491337000919429"
   let callback_url = "https://keigoimai.info/scribble/callback"
 end
 
-let acceptor, hook = http_acceptor ~base_path:"/scribble"
-let () = Cohttp_server_lwt.hook := hook
-
+(* prepare a HTTP server *)
+let acceptor, hook = H.http_acceptor ~base_path:"/scribble"
 let () =
+  Cohttp_server_lwt.hook := hook;
   Lwt.async (Cohttp_server_lwt.start_server "/tmp/foo" 8080 "127.0.0.1" "index.html" None)
-
-let swap (k1,k2) = k2, k1
 
 let u = a
 let p = b
 
 let mk_oauth () =
-  (u -!-> c) (O.get "/oauth") @@ (fun kuc ->
-  (c --> u) (O._302 (swap kuc)) @@
+  (u -!-> c) (H.get "/oauth") @@ (fun kuc ->
+  (c --> u) (H._302 (swap kuc)) @@
   discon u c kuc @@
-  (u -!-> p) (O.get "-TODO-") @@ (fun kup ->
-  (p --> u) (O._200 (swap kup)) @@
+  (u -!-> p) (H.get "-TODO-") @@ (fun kup ->
+  (p --> u) (H._200 (swap kup)) @@
   discon u p kup @@
-  (u -!-> p) (O.post "-TODO-") @@ (fun kup ->
-  (p -%%-> u) (O.success_fail (fun _ -> failwith "") (fun _ -> failwith "") kup)
+  (u -!-> p) (H.post "-TODO-") @@ (fun kup ->
+  (p -%%-> u) (H._200_success_fail (fun _ -> failwith "") (fun _ -> failwith "") kup)
     ~l1:((p,u), discon u p kup @@
-                (u -!-> c) (O.success ~pred:(fun c -> Util.http_parameter_contains ("state", c.O.sessionid)) "/callback") (fun kuc ->
-                (c -!-> p) (O.get "/access_token") (fun kcp ->
-                (p --> c) (O._200 (swap kcp)) @@
+                (u -!-> c) (H.success ~pred:(fun c -> H.Util.http_parameter_contains ("state", c.H.extra_server)) "/callback") (fun kuc ->
+                (c -!-> p) (H.get "/access_token") (fun kcp ->
+                (p --> c) (H._200 (swap kcp)) @@
                 discon c p kcp @@
-                (c --> u) (O._200 (swap kuc)) @@
+                (c --> u) (H._200 (swap kuc)) @@
                 discon u c kuc @@
                 finish)))
     ~l2:((p,u), discon u p kup @@
-                (u -!-> c) (O.fail ~pred:(fun _ -> Util.http_parameter_contains ("error", "access_denied")) "/callback") (fun kuc ->
-                (c --> u) (O._200 (swap kuc)) @@
+                (u -!-> c) (H.fail ~pred:(fun _ -> H.Util.http_parameter_contains ("error", "access_denied")) "/callback") (fun kuc ->
+                (c --> u) (H._200 (swap kuc)) @@
                 discon u c kuc @@
                 finish)))))
 
@@ -58,9 +54,9 @@ let facebook_oauth () =
   print_endline "oauth_consumer started";
   let g = mk_oauth () in
   let s = get_sess c g in
-  acceptor () >>= fun srv ->
+  acceptor "" >>= fun srv ->
   let sessionid = Int64.to_string @@ Random.int64 Int64.max_int in
-  let srv = {O.sessionid; body=srv} in
+  srv.H.extra_server <- sessionid;
   accept A srv s >>= fun (`get(params, s)) ->
   print_endline "connection accepted";
   let redirect_url =
@@ -71,21 +67,21 @@ let facebook_oauth () =
        ("state", sessionid)]
   in
   let s = send A (fun x->x#_302) redirect_url s in
-  disconnect A (fun k -> k.O.body.close_server ()) s >>= fun s ->
-  acceptor () >>= fun srv' ->
-  accept A {srv with O.body=srv'} s >>= function
+  disconnect A (fun k -> k.H.close_server ()) s >>= fun s ->
+  acceptor sessionid >>= fun srv' ->
+  accept A srv s >>= function
   | `success(_,s) ->
-     http_connector ~base_url:"https://graph.facebook.com/v2.11/oauth" >>= fun cli ->
-     let s = request B (fun x->x#get) [] {O.sessionid=""; body=cli} s in
+     H.http_connector ~base_url:"https://graph.facebook.com/v2.11/oauth" sessionid >>= fun cli ->
+     let s = request B (fun x->x#get) [] cli s in
      receive B s >>= fun (`_200(_,s)) ->
-     disconnect B (fun k -> k.O.body.close_client ()) s >>= fun s ->
+     disconnect B (fun k -> k.H.close_client ()) s >>= fun s ->
      let s = send A (fun x->x#_200) "auth succeeded" s in
-     disconnect A (fun k -> k.O.body.close_server ()) s >>= fun s ->
+     disconnect A (fun k -> k.H.close_server ()) s >>= fun s ->
      close s;
      Lwt.return ()
   | `fail(_,s) ->
      let s = send A (fun x->x#_200) "auth failed" s in
-     disconnect A (fun k -> k.O.body.close_server ()) s >>= fun s ->
+     disconnect A (fun k -> k.H.close_server ()) s >>= fun s ->
      close s;
      Lwt.return ()
 
--- lib/base.ml
+++ lib/base.ml
@@ -4,3 +4,5 @@ type ('v1, 'v2, 's1, 's2) lens = {
     get : 's1 -> 'v1;
     put : 's1 -> 'v2 -> 's2;
   }
+
+let swap (k1,k2) = k2, k1

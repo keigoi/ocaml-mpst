640e963b507cff0c6e9c175cc220afe86242caf9
Keigo Imai <keigo.imai@gmail.com>
2018-11-21 11:43:35 +0900

    works (explicit connection)

--- light/poly_conn.ml
+++ light/poly_conn.ml
@@ -35,24 +35,32 @@ end
 
 module Local : sig
   type ('a, 'b) either = Left of 'a | Right of 'b
-  type ('s,'v,'r) send = DummySend of 's * 'v * 'r
-  type ('s,'v,'r) recv = DummyRecv of 's * 'v * 'r
-  type ('ss,'r) select = DummySelect of 'ss * 'r
-  type ('ss,'r) offer = DummyOffer of 'ss * 'r
+  type ('s,'v,'r) send = DummySend
+  type ('s,'v,'r) recv = DummyRecv
+  type ('ss,'r) select = DummySelect
+  type ('ss,'r) offer = DummyOffer
+  type ('s,'ks,'r) connect = DummyConnect
   type close
   type ('k, 'r) conn = DummyConn
 
   type (_,_) prot =
-  | Send : 'r * ('k -> 'v Channel.o) * ('ks -> ('ks,'s) sess) -> ('ks, ('s, 'v, ('k,'r) conn) send) prot
+  | Connect : 'r * ('ks2 -> ('ks2,'s) sess) -> ('ks, ('s, 'ks2, 'r) connect) prot
+  | Send : 'r * ('k -> 'v -> unit) * ('ks -> ('ks,'s) sess) -> ('ks, ('s, 'v, ('k,'r) conn) send) prot
   | Recv : 'r * (('k -> 'v Channel.i) * ('ks -> ('ks, 's) sess)) list -> ('ks, ('s, 'v, ('k,'r) conn) recv) prot
   | Select : 'r * ('ks -> 'k -> 'ss) -> ('ks,('ss, ('k,'r) conn) select) prot
   | Offer : 'r * ('ks -> 'k -> 'ss Channel.i) list -> ('ks, ('ss, ('k,'r) conn) offer) prot
   | Close : ('ks, close) prot
-
-  and ('ks,'c) sess = Sess of 'ks * ('ks, 'c) prot
+  and ('ks,'c) sess =
+    Sess of 'ks * ('ks, 'c) prot
 
   val init: 'ks -> ('ks,'s) prot -> ('ks, 's) sess
 
+  val connect :
+        <role:'r; lens:(unit,'k,'ks1,'ks2) plens; ..> ->
+        'k ->
+        ('ks1, ('s, 'ks2, 'r) connect) sess ->
+        ('ks2, 's) sess
+      
   val send : <role:'r; lens:('k,'ks) lens; ..> -> 'v -> ('ks, ('s,'v,('k, 'r) conn) send) sess -> ('ks,'s) sess
   val receive : <role:'r; lens:('k,'ks) lens; ..> -> ('ks,('s,'v,('k,'r) conn) recv) sess -> 'v * ('ks,'s) sess
     
@@ -78,30 +86,41 @@ module Local : sig
 
 end = struct
 
-  type ('a,'b) either = Left of 'a | Right of 'b
-
-  type ('s,'v,'r) send = DummySend of 's * 'v * 'r
-  type ('s,'v,'r) recv = DummyRecv of 's * 'v * 'r
-  type ('ss,'r) select = DummySelect of 'ss * 'r
-  type ('ss,'r) offer = DummyOffer of 'ss * 'r
+  type ('a, 'b) either = Left of 'a | Right of 'b
+  type ('s,'v,'r) send = DummySend
+  type ('s,'v,'r) recv = DummyRecv
+  type ('ss,'r) select = DummySelect
+  type ('ss,'r) offer = DummyOffer
+  type ('s,'ks,'r) connect = DummyConnect
   type close
   type ('k, 'r) conn = DummyConn
 
   type (_,_) prot =
-  | Send : 'r * ('k -> 'v Channel.o) * ('ks -> ('ks,'s) sess) -> ('ks, ('s, 'v, ('k,'r) conn) send) prot
+  | Connect : 'r * ('ks2 -> ('ks2,'s) sess) -> ('ks, ('s, 'ks2, 'r) connect) prot
+  | Send : 'r * ('k -> 'v -> unit) * ('ks -> ('ks,'s) sess) -> ('ks, ('s, 'v, ('k,'r) conn) send) prot
   | Recv : 'r * (('k -> 'v Channel.i) * ('ks -> ('ks, 's) sess)) list -> ('ks, ('s, 'v, ('k,'r) conn) recv) prot
   | Select : 'r * ('ks -> 'k -> 'ss) -> ('ks,('ss, ('k,'r) conn) select) prot
   | Offer : 'r * ('ks -> 'k -> 'ss Channel.i) list -> ('ks, ('ss, ('k,'r) conn) offer) prot
   | Close : ('ks, close) prot
-  and ('ks,'c) sess = Sess of 'ks * ('ks, 'c) prot
+  and ('ks,'c) sess =
+    Sess of 'ks * ('ks, 'c) prot
 
   let init ks p = Sess (ks, p)
-    
+                
+  type none
+  let connect : 'r 'k 'ks1 'ks2.
+        <role:'r; lens:(unit,'k,'ks1,'ks2) plens; ..> ->
+        'k ->
+        ('ks1, ('s, 'ks2, 'r) connect) sess ->
+        ('ks2, 's) sess =
+          fun ridx k (Sess (ks, Connect (r, f))) ->
+          let ks2 = ridx#lens.put ks k in
+          f ks2
+          
   let send =
-    fun ridx v (Sess (ks,Send (_,f,cont))) ->
+    fun ridx v (Sess (ks,Send (_,writer,cont))) ->
     let k = ridx#lens.get ks in
-    let ch = f k in
-    Channel.send ch v;
+    writer k v;
     cont ks
           
   let receive =
@@ -140,17 +159,17 @@ end = struct
   end
 end
 
-module Global : sig
+module Global: sig
   open Local
-  type ('k1,'k2,'v) channel = {receiver: 'k1 -> 'v Channel.i; sender: 'k2 -> 'v Channel.o}
+  type ('k1,'k2,'v) channel = {receiver: 'k1 -> 'v Channel.i; sender: 'k2 -> 'v -> unit}
   type ('k1,'k2) medium = {instance : 'v. unit -> ('k1,'k2,'v) channel}
   
   val ( ==> ) :
         < role : 'ra;
-          lens : (('ks,'sa) prot, ('ks, ('sa, 'v, ('k2, 'rb) conn) send) prot, 'c0, 'c1) plens;
+          lens : (('ksa,'sa) prot, ('ksa, ('sa, 'v, ('k2, 'rb) conn) send) prot, 'c0, 'c1) plens;
           ext : 'ms -> 'ms0; .. > ->
         < role : 'rb;
-          lens : (('ks, 'sb) prot, ('ks, ('sb, 'v, ('k1, 'ra) conn) recv) prot, 'c1, 'c2) plens;
+          lens : (('ksb, 'sb) prot, ('ksb, ('sb, 'v, ('k1, 'ra) conn) recv) prot, 'c1, 'c2) plens;
           ext : 'ms0 -> ('k1, 'k2) medium; .. > ->
         ('ms -> 'c0) -> 'ms -> 'c2
 
@@ -193,10 +212,17 @@ module Global : sig
       merge : 'c1 -> 'c1 -> 'c1; .. > * ('ms -> 'c0l) ->
     < lens : (('ks, ('r, ('k, 'ra) conn) select) prot, ('ks, close) prot, 'c0r, 'c1) plens; .. > * ('ms -> 'c0r) ->
     'ms -> 'c2
+
+  val (=!=>) : 
+    < role : 'ra;
+      lens : (('ks11, 'sa) prot, ('ks1, ('sa, 'ks11, 'rb) connect) prot, 'c0, 'c1) plens; .. > ->
+    < role : 'rb; 
+      lens : (('ks22, 'sb) prot, ('ks2, ('sb, 'ks22, 'ra) connect) prot, 'c1, 'c2) plens; .. > ->
+    ('ms -> 'c0) -> 'ms -> 'c2
                                                                                       
-end = struct
+end =  struct
   open Local
-  type ('k1,'k2,'v) channel = {receiver: 'k1 -> 'v Channel.i; sender: 'k2 -> 'v Channel.o}
+  type ('k1,'k2,'v) channel = {receiver: 'k1 -> 'v Channel.i; sender: 'k2 -> 'v -> unit}
   type ('k1,'k2) medium = {instance : 'v. unit -> ('k1,'k2,'v) channel}
      
   type ('ks1, 'ks2, 'sa,'sb,'sa0,'sb0) label1 =
@@ -204,11 +230,11 @@ end = struct
                       -> ('ks1,('sa, ('k2,'rb) conn) select) prot * ('ks2,('sb, ('k1,'ra) conn) offer) prot)}
 
   let left = {label1=(fun c ((ra,sa), (rb,sb)) ->
-                Select (rb, fun ks k -> object method left=Channel.send (c.sender k) (); Sess (ks, Lazy.force sa) end),
+                Select (rb, fun ks k -> object method left=c.sender k (); Sess (ks, Lazy.force sa) end),
                 Offer (ra, [(fun ks k -> Channel.map (c.receiver k) (fun _ -> `left ((Sess (ks, Lazy.force sb)))))])
              )}
   let right = {label1=(fun c ((ra,sa), (rb,sb)) ->
-                Select (rb, fun ks k -> object method right=Channel.send (c.sender k) (); Sess (ks, Lazy.force sa) end),
+                Select (rb, fun ks k -> object method right=c.sender k (); Sess (ks, Lazy.force sa) end),
                 Offer (ra, [(fun ks k -> Channel.map (c.receiver k) (fun _ -> `right ((Sess (ks, Lazy.force sb)))))])
              )}
 
@@ -262,6 +288,22 @@ end = struct
     and c1 = lazy (a#lens.put c0 (fst (Lazy.force sab'))) in
     let c2 = b#lens.put (Lazy.force c1) (snd (Lazy.force sab')) in
     c2
+  
+  let (=!=>) : 'ra 'ra 'ks1 'ks11 'ks2 'ks22 'sa 'sb 'c0 'c1 'c2 'ms.
+    < role : 'ra;
+      lens : (('ks11, 'sa) prot, ('ks1, ('sa, 'ks11, 'rb) connect) prot, 'c0, 'c1) plens; .. > ->
+    < role : 'rb; 
+      lens : (('ks22, 'sb) prot, ('ks2, ('sb, 'ks22, 'ra) connect) prot, 'c1, 'c2) plens; .. > ->
+    ('ms -> 'c0) -> 'ms -> 'c2
+        = fun a b c0 ms ->
+    let c0 = c0 ms in
+    let sa = lazy (a#lens.get c0) in
+    let rec sb = lazy (b#lens.get (Lazy.force c1)) 
+    and sab' = lazy (Connect (b#role, (fun ks -> Sess (ks, Lazy.force sa))),
+                     Connect (a#role, (fun ks -> Sess (ks, Lazy.force sb))))
+    and c1 = lazy (a#lens.put c0 (fst (Lazy.force sab'))) in
+    let c2 = b#lens.put (Lazy.force c1) (snd (Lazy.force sab')) in
+    c2
 end
 
 module ThreeParty = struct
@@ -291,6 +333,8 @@ include Local
 
 module Example1 = struct
   let global_example () =
+    (a =!=> b) @@
+    (b =!=> c) @@
     (a ==> b) @@
     (b ==> c) @@
     choice_at a left_or_right
@@ -304,6 +348,7 @@ module Example1 = struct
 
   let t1 sa =
     let s = sa in
+    let s = connect b () s in
     let s = send b 100 s in
     if Random.bool () then
       let s = select_left b s in
@@ -314,8 +359,10 @@ module Example1 = struct
       Printf.printf "A: received: %s\n" v;
       close s
       
-  let t2 sb =
+  let t2 bc_channel sb =
     let s = sb in
+    let s = connect a () s in
+    let s = connect c bc_channel s in
     let v,s = receive a s in
     Printf.printf "B: received: %d\n" v;
     let s = send c "Hello" s in
@@ -328,8 +375,9 @@ module Example1 = struct
        let s = send c "to C (2)" s in
        close s
        
-  let t3 sc =
+  let t3 bc_channel sc =
     let s = sc in
+    let s = connect b bc_channel s in
     let v,s = receive b s in
     Printf.printf "C: received: %s\n" v;
     let w,s = receive b s in
@@ -337,19 +385,42 @@ module Example1 = struct
     close s
 
   let shmem =
-    let m = {instance=(fun () -> let o,i = Channel.create () in {receiver=(fun () -> i); sender=(fun () -> o)})} in
+    let m = {instance=(fun () -> let o,i = Channel.create () in {receiver=(fun () -> i); sender=(fun () -> Channel.send o)})} in
     let make_ab p = object method a=p method b=p end in
     let make_bc p = object method b=p method c=p end in
     let make_ca p = object method a=p method c=p end in
     object method a=make_bc m method b=make_ca m method c=make_ab m end
+
+  let make :
+        ('kab,'kba) medium * ('kba,'kab) medium ->
+        ('kbc,'kcb) medium * ('kcb,'kbc) medium ->
+        ('kca,'kac) medium * ('kac,'kca) medium ->
+        _
+    = fun ab bc ca ->
+    let (!!) (a,_) = a in
+    let (!?) (_,a) = a in
+    let make_ab v1 v2 = object method a=v1 method b=v2 end in
+    let make_bc v1 v2 = object method b=v1 method c=v2 end in
+    let make_ca v1 v2 = object method c=v1 method a=v2 end in
+    object method a=make_bc !!ab !?ca method b=make_ca !!bc !?ab method c=make_ab !!ca !?bc end
+
+  let ac_stream =
+    let m = {instance=(fun () ->
+               let o,i = Channel.create () in
+               {receiver=(fun () -> i); sender=(fun () -> Channel.send o)})} in
+    let st = {instance=(fun () ->
+                {receiver=(fun (o,i) -> Channel.map i (fun v -> Marshal.from_bytes v 0));
+                 sender=(fun (o,i) v -> Channel.send o (Marshal.to_bytes v []))})} in
+    make (m,m) (st,st) (m,m)
     
   let main () =
     Random.self_init ();
-    let g = global_example () shmem in
+    let g = global_example () ac_stream in
     let sa, sb, sc = a#lens.get g, b#lens.get g, c#lens.get g in
+    let (o1,i1), (o2,i2) = Channel.create (), Channel.create () in
     let t1id = Thread.create (fun () -> t1 (init ((),(),()) sa)) () in
-    let t2id = Thread.create (fun () -> t2 (init ((),(),()) sb)) () in
-    t3 (init ((),(),()) sc);
+    let t2id = Thread.create (fun () -> t2 (o1,i2) (init ((),(),()) sb)) () in
+    t3 (o2,i1) (init ((),(),()) sc);
     Thread.join t1id;
     Thread.join t2id;
     ()

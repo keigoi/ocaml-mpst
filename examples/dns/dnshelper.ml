b71f286136fab8a6187d7f22d6ec2fd6fc5080e6
Keigo Imai <keigo.imai@gmail.com>
2018-11-29 22:57:34 +0900

    fix

--- examples/dns/dnshelper.ml
+++ examples/dns/dnshelper.ml
@@ -1,10 +1,8 @@
-open Mpst.ThreeParty
+open Mpst.Global
+open Mpst.Session
 open Lwt
-open Dns
 
 let sp = Printf.sprintf
-type t = {fd:Lwt_unix.file_descr;
-          mutable query : Packet.t (* query *) }
 
 let dummy_or_query =
   {label_merge=(fun l r -> object method dummy=l#dummy method query=r#query end)}
@@ -20,44 +18,38 @@ let query =
   {select_label=(fun f -> object method query=f end);
    offer_label=(fun l -> `query l);
    channel=
-     {sender=(fun (Conn ({fd;_} as r)) (dst, query) ->
-        r.query <- query;
-        let buf = Packet.marshal query in
+     {sender=(fun (Conn fd) (dst_addr, query) -> (* XXX move dstaddr to connection type??*)
+        let buf = Dns.Packet.marshal query in
         Lwt.async (fun () ->
-            Cstruct.(Lwt_bytes.sendto fd buf.buffer buf.off buf.len [] dst)
+            Cstruct.(Lwt_bytes.sendto fd buf.buffer buf.off buf.len [] dst_addr)
         )
       );
-      receiver=(fun (Conn {fd;_}) ->
+      receiver=(fun (Conn fd) ->
         let buf = Cstruct.create 4096 in
         Cstruct.(Lwt_bytes.recvfrom fd buf.buffer buf.off buf.len [])
-        >>= fun (len, dst) ->
+        >>= fun (len, dst_addr) ->
         match Dns.Protocol.Server.parse (Cstruct.sub buf 0 len) with
         | None -> Lwt.choose []
         | Some query ->
-           Lwt.return (dst, query)
+           Lwt.return (dst_addr, query)
   )}}
 
 let answer =
   {select_label=(fun f -> object method answer=f end);
    offer_label=(fun l -> `answer l);
    channel=
-     {sender=(fun (Conn {fd;_}) (dst, query, answer) ->
-        let resp = Dns.Query.response_of_answer query answer in
-        match Dns.Protocol.Server.marshal query resp with
-        | None -> failwith "marshal failed"
-        | Some buf ->
-           Lwt.async (fun () ->
-               Cstruct.(Lwt_bytes.sendto fd buf.buffer buf.off buf.len [] dst)
-             )
+     {sender=(fun (Conn fd) (dst_addr, resp) ->
+        let buf = Dns.Packet.marshal resp in
+        Lwt.async (fun () ->
+            Cstruct.(Lwt_bytes.sendto fd buf.buffer buf.off buf.len [] dst_addr)
+          )
       );
-      receiver=(fun (Conn {fd;query;_}) ->
+      receiver=(fun (Conn fd) ->
         let buf = Cstruct.create 4096 in
         Cstruct.(Lwt_bytes.recvfrom fd buf.buffer buf.off buf.len [])
-        >>= fun (len, dst) ->
+        (* FIXME: check query id -- we need session correlation!! *)
+        >>= fun (len, dst_addr) ->
         let buf = Cstruct.sub buf 0 len in
-        let p = Packet.parse buf in
-        let answer = Dns.Query.answer_of_response p in
-        Lwt.return (dst,query,answer))}
+        let answer = Dns.Packet.parse buf in
+        Lwt.return (dst_addr, answer))}
   }
-
-
--- examples/dns/dnsserver.ml
+++ examples/dns/dnsserver.ml
@@ -1,17 +1,18 @@
-open Mpst.ThreeParty
-open Lwt
-open Dns
-open Dnshelper
+module S = Mpst.Session
+let (>>=) = Lwt.(>>=)
 
 (* FIXME!!! *)
-let myip = "172.21.24.215"
+let myip = "10.0.1.11"
 let myport = 12345
-   
+
+let forward_ip = "8.8.8.8"
+let forward_port = 53
+
 (*
   $ opam install dns-lwt-unix
   $ dune build --profile=release examples/dns/dnsserver.exe
   $ sudo _build/default/examples/dns/dnsserver.exe
-  
+
   (in another window)
   $ host nagoya.my.domain 127.0.0.1
  *)
@@ -19,21 +20,23 @@ let myport = 12345
 type cli = Cli
 type srv = Srv
 type oth = Oth
-let cli = {a with role=Cli}
-let srv = {b with role=Srv}
-let otr = {c with role=Oth}
+let cli = {Mpst.ThreeParty.a with role=Cli}
+let srv = {Mpst.ThreeParty.b with role=Srv}
+let fwd = {Mpst.ThreeParty.c with role=Oth}
 
 
-        
+
 let dns () =
+  let open Mpst.ThreeParty in
+  let open Dnshelper in
   ((cli,cli) -!-> (srv,srv)) query @@
   choice_req_at srv dummy_or_query
-    (srv, ((srv,srv) -!-> (otr,otr)) dummy @@
-          discon (srv,srv) (otr,otr) @@
+    (srv, ((srv,srv) -!-> (fwd,fwd)) dummy @@
+          discon (srv,srv) (fwd,fwd) @@
           ((srv,srv) -?-> (cli,cli)) answer @@
           finish_)
-    (srv, ((srv,srv) -!-> (otr,otr)) query @@
-          ((otr,otr) -?-> (srv,srv)) answer @@
+    (srv, ((srv,srv) -!-> (fwd,fwd)) query @@
+          ((fwd,fwd) -?-> (srv,srv)) answer @@
           ((srv,srv) -?-> (cli,cli)) answer @@
           finish_)
 
@@ -56,59 +59,59 @@ let lookup packet =
       let rrs = [ { name = q_name; cls = RR_IN; flush = false; ttl = 0l; rdata = A ip } ] in
       Lwt.return (Some (Dns.Query.({ rcode = NoError; aa = true; answer = rrs; authority = []; additional = [] })))
     end else begin
-      Lwt_io.printf "DNS: %s returning NXDOMAIN\n" (to_string packet)
-      >>= fun () ->
       Lwt.return None
     end
   | _ ->
-    Lwt_io.printf "DNS: %s returning NXDOMAIN\n" (to_string packet)
-    >>= fun () ->
     Lwt.return None
 
 let sockaddr addr port =
   Lwt_unix.(ADDR_INET (Ipaddr_unix.to_inet_addr addr, port))
 
-let server fd fd2 =
-  let s = get_sess_ srv (dns ()) in
+let server fd_cli fd_other =
+  let s = Mpst.ThreeParty.get_sess_ srv (dns ()) in
   let rec loop () =
-    accept cli fd s >>= fun (`query((dst, query), s)) ->
+    S.accept cli fd_cli s >>= fun (`query((cli_addr, query), s)) ->
     lookup query >>= fun answer ->
     begin match answer with
     | Some answer ->
-       let s = request otr (fun x->x#dummy) () fd s in
-       let s = disconnect otr s in
-       Lwt.return (dst, query, answer, s)
+       let s = S.request fwd (fun x->x#dummy) () fd_cli s in (* no effect *)
+       let s = S.disconnect fwd s in (* no effect *)
+       Lwt.return (answer, s)
     | None ->
-       let fwd = sockaddr (Ipaddr.of_string_exn "8.8.8.8") 53 in
-       let open Packet in
        match query.questions with
-       | [] -> failwith "no question"
-       | q::_ ->
-       let query2 = Dns.Query.create ~id:12345 ~dnssec:false q.q_class q.q_type q.q_name in
-       let s = request otr (fun x->x#query) (fwd,query2) fd2 s in
-       receive otr s >>= fun (`answer((_dst,_query,answer),s)) ->
-       let s = disconnect otr s in
-       Lwt.return (dst,query,answer, s)
-    end >>= fun (dst,query,answer,s) ->
-    let s = send cli (fun x->x#answer) (dst,query,answer) s in
-    let s = disconnect cli s in
-    close s;
+       | [] ->
+          let s = S.request fwd (fun x->x#dummy) () fd_cli s in (* no effect *)
+          let s = S.disconnect fwd s in (* no effect *)
+          Lwt.return (nxdomain, s)
+       | q::_ -> (* FIXME: discarding rest of queries *)
+          let fwd_query =
+            Dns.Query.create
+              ~id:(Random.int (1 lsl 16))
+              ~dnssec:false
+              q.q_class q.q_type q.q_name
+          in
+          let fwd_addr = sockaddr (Ipaddr.of_string_exn forward_ip) forward_port in
+          let s = S.request fwd (fun x->x#query) (fwd_addr, fwd_query) fd_other s in
+          S.receive fwd s >>= fun (`answer((_dst, fwd_resp),s)) ->
+          let fwd_answer = Dns.Query.answer_of_response fwd_resp in
+          let s = S.disconnect fwd s in
+          Lwt.return (fwd_answer, s)
+    end >>= fun (answer, s) ->
+    let resp = Dns.Query.response_of_answer query answer in
+    let s = S.send cli (fun x->x#answer) (cli_addr, resp) s in
+    let s = S.disconnect cli s in
+    S.close s;
     loop ()
   in
   loop ()
 
-let dummy_query =
-    let open Packet in
-    let detail = {qr=Query; opcode=Standard; aa=false; tc=false; rd=false; ra=false; rcode=NoError} in
-    {id=0; detail; questions=[]; answers=[]; authorities=[]; additionals=[]}
-
 let main () =
   Dns_server_unix.bind_fd ~address:"127.0.0.1" ~port:53
-  >>= fun (fd, _src) ->
+  >>= fun (fd_cli, _src) ->
   Dns_server_unix.bind_fd ~address:myip ~port:myport
-  >>= fun (fd2, _src) ->
-  server {fd;query=dummy_query} {fd=fd2;query=dummy_query}
+  >>= fun (fd_fwd, _src) ->
+  server fd_cli fd_fwd
 
 let () =
+  Random.self_init ();
   Lwt_main.run (main ())
-      

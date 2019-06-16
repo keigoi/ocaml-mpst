008243397c3a127cc963dde781e07d9103b95a1e
Keigo Imai <keigo.imai@gmail.com>
2019-06-16 15:31:18 +0100

    documentation

--- lib/common/mpst_common.ml
+++ lib/common/mpst_common.ml
@@ -19,27 +19,27 @@ type ('la,'va) method_ =
 
 (**
  * A mergeable is a session endpoint which can be merged with another endpoint in future.
- * It is a function of type `('a option -> 'a)` which returns (1) the merged endpoint when
- * another endpoint `(Some ep)` is passed, or (2) the endpoint itself when the argument is `None`.
+ * A mergeble is a bundle of an endpoint and its merging strategy, providing a way 
+ * to merge two endpoints of the same type into one.
  * 
  * Mergeables enable endpoints to be merged based on the object structure.
  * In ocaml-mpst, endpoints are nested objects (and events).
- * The problem is that, in OCaml type system, one can not inspect the object structure 
- * when its type is not known.
- * A mergeble is a bundle of an endpoint and its merging strategy, providing a way 
- * to merge two endpoints of the same type into one.
+ * The problem is that, in OCaml type system, one can not inspect the object structure
+ * when its type is not known, and because of this, we can not merge structural objects. 
+ * (In this case, I think GADTs will not help.) 
+ * As mergeables have both merging strategy and its value at the same place,
+ * it enables values to be merged with other values if they have the same type.
  *
  * Mergeables themselves can be merged with other mergeables using `Mergeable.merge`.
  *
  * Mergeables can be "delayed" by using `Mergeable.make_recvar`. Delayed mergeables are used
  * to encode recursive endpoints. Merging delayed mergeable will also generate a delayed 
  * mergeable.
- * A delayed mergeable are forced when `resolve_merge` is called.
+ * A delayed mergeable are forced when it is actually extracted by `Mergeable.out`.
  *
- * In ocaml-mpst, `resolve_merge` is called during "get_ep" phase to ensure that the all
- * mergings are resolved before actual communication will take place.
- *
- * Mergeable can be created with hooks. 
+ * In ocaml-mpst, call of `Mergeable.out` is chained during "get_ep" phase to ensure that 
+ * the all mergings are resolved before actual communication will take place.
+ * It is realised by the hooks -- see the implementation of (-->) combinator, for example.
  *)
 module Mergeable :
 sig

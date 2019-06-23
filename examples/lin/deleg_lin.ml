7161d9730b32fda1ec112a8bb0ccbfed40d7b8b5
Keigo Imai <keigo.imai@gmail.com>
2019-06-23 04:21:31 +0100

    delegation example

--- examples/lin/deleg_lin.ml
+++ examples/lin/deleg_lin.ml
@@ -1,18 +1,15 @@
 open Mpst_monad
 open Linocaml
-open Calc_util
+module Util = Calc_util.Make(Mpst_monad.Nocheck.Nodyncheck)
+open Util
 
 let mst = {role_index=Zero;
            role_label={make_obj=(fun v->object method role_Mst=v end);
                        call_obj=(fun o -> o#role_Mst)}}
 
-(* let to_srv m =
- *   {obj_merge=(fun l r -> object method role_Srv=m.obj_merge l#role_Srv r#role_Srv end);
- *   obj_splitL=(fun lr -> (lr :> <>))} *)
-
 
 type op = Add | Sub | Mul | Div
-let calc () =
+let calc =
   fix (fun t ->
       choice_at cli (to_srv compute_or_result)
            (cli, (cli --> srv) compute @@
@@ -21,15 +18,16 @@ let calc () =
                  (srv --> cli) answer @@
                  finish))
 
-let worker () =
+let worker =
   (srv --> mst) msg @@
   (mst --> srv) msg @@
   finish
 
 let s = Linocaml.Zero
-let t = Linocaml.Succ Linocaml.Zero
+let t = Linocaml.(Succ Zero)
 
 let tCli_monad () =
+  print_endline "client start";
   let%lin #s = s <@ send (fun x->x#role_Srv#compute) (Add, 20) in
   let%lin #s = s <@ send (fun x->x#role_Srv#compute) (Sub, 45) in
   let%lin #s = s <@ send (fun x->x#role_Srv#compute) (Mul, 10) in
@@ -44,22 +42,22 @@ let tSrv_monad () =
   let rec loop acc =
     match%lin s <@ receive (fun x->x#role_Cli) with
     | `compute({data=(sym,num)}, #s) ->
-      let op = match sym with
-        | Add -> (+)   | Sub -> (-)
-        | Mul -> ( * ) | Div -> (/)
-      in loop (op acc num)
+       let op = match sym with
+         | Add -> (+)   | Sub -> (-)
+         | Mul -> ( * ) | Div -> (/)
+       in loop (op acc num)
     | `result(_, #s) ->
-      let%lin #s = s <@ send (fun x->x#role_Cli#answer) acc in
-      s <@ close
+       let%lin #s = s <@ send (fun x->x#role_Cli#answer) acc in
+       s <@ close
   in loop 0
 
-let calc_sh = create_shared calc [`role_Cli(); `role_Srv()]
-let work_sh = create_shared worker [`role_Mst(); `role_Srv()]
+let calc_sh = create_shared ~kinds:[IPCProcess;IPCProcess] calc
+let work_sh = create_shared ~kinds:[Untyped;Untyped] worker
 
 let tSrvWorker i =
-  print_endline "worker started";
   let rec loop () =
-    let%lin #t = connect work_sh srv in
+    print_endline "worker loop";
+    let%lin #t = accept work_sh srv in
     let%lin #t = t <@ send (fun x->x#role_Mst#msg) i in
     let%lin `msg(#s, #t) = t <@ receive (fun x->x#role_Mst) in
     t <@ close >>= fun () ->
@@ -68,41 +66,56 @@ let tSrvWorker i =
   in loop ()
 
 let tMaster () =
-  print_endline "master started";
   let rec loop () =
-    let%lin #s = connect calc_sh srv in
+    print_endline "master loop";
+    let%lin #s = accept calc_sh srv in
     print_endline "master: client comes";
     let%lin #t = connect work_sh mst in
     let%lin `msg(i, #t) = t <@ receive (fun x->x#role_Srv) in
     Printf.printf "master: connecrted to a worker %d\n" i;
-    let%lin #t = (s @* t) <@ deleg_send (fun x->x#role_Srv#msg) in
+    let%lin #t = (t @* s) <@ deleg_send (fun x->x#role_Srv#msg) in
     t <@ close >>= loop
   in loop ()
 
-let run f x =
+let run f x : unit =
   Linocaml.run' f x
 
 let repeat cnt f =
   let rec loop cnt acc =
     if cnt > 0 then begin
-      let v = f cnt in
-      loop (cnt-1) (v::acc)
-    end else
+        let v = f cnt in
+        loop (cnt-1) (v::acc)
+      end else
       acc
   in
   loop cnt []
 
+let rec tSrv_monad () =
+  let%lin #s = accept calc_sh srv in
+  let rec loop acc =
+    match%lin s <@ receive (fun x->x#role_Cli) with
+    | `compute({data=(sym,num)}, #s) ->
+      let op = match sym with
+        | Add -> (+)   | Sub -> (-)
+        | Mul -> ( * ) | Div -> (/)
+      in loop (op acc num)
+    | `result({data=()}, #s) ->
+      let%lin #s = s <@ send (fun x->x#role_Cli#answer) acc in
+      s <@ close >>= fun () ->
+      tSrv_monad ()
+  in loop 0
+
 
 let _ : Thread.t list =
   repeat 10 (fun i ->
-      Thread.create (fun () -> run (fun () -> tSrvWorker i)) ())
+      Thread.create (run (fun () -> tSrvWorker i)) ())
 
 let _ : Thread.t =
-  Thread.create (fun () -> run tMaster) ()
+  Thread.create (run tMaster) ()
 
 let () =
   List.iter Thread.join @@
-                    repeat 10 (fun i -> print_endline"here"; Thread.create (run (fun () ->
-                                            Printf.printf "%d\n" i;
-                                            let%lin #s = connect calc_sh cli in
-                                            tCli_monad ())) ())
+    repeat 10 (fun i -> Thread.create (fun () ->
+                            run (fun () ->
+                                let%lin #s = connect calc_sh cli in
+                                tCli_monad ()) ()) ())

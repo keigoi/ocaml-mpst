9a09de3eaa627bd189b4219f48b5d99dc4106964
Keigo Imai <keigo.imai@gmail.com>
2019-06-23 07:00:41 +0100

    scatter/gather example

--- examples/simple/ex2.ml
+++ examples/simple/ex2.ml
@@ -1,54 +1,46 @@
 (* multicasts *)
-open Mpst_shmem.Session
-open Mpst_shmem.Global
-open Mpst_shmem.Util
-   
-let a : ([`A],_,_,_,_) role = {role=`A; lens=Fst}
-let b : ([`B],_,_,_,_) role = {role=`B; lens=Next Fst}
-let c : ([`C],_,_,_,_) role = {role=`C; lens=Next (Next Fst)}
-
-let finish = many_at b 10 (one @@ one @@ one @@ nil)
+open Mpst_lwt
 
 let (>>=) = Lwt.(>>=)
 
 (* A global protocol between A, B, and C *)
-let create_g () =
+let prot =
     (c --> a) msg @@
-    choice_at a left_or_right
-      (a, (a -->> b) left @@
-          (b >>-- c) right @@
-          (b >>-- a) msg @@
+    choice_at a (to_b left_or_right)
+      (a, (scatter a b) left @@
+          (gather b c) right @@
+          (gather b a) msg @@
           finish)
-      (a, (a -->> b) right @@
-          (b >>-- a) msg @@
-          (b >>-- c) left @@
+      (a, (scatter a b) right @@
+          (gather b a) msg @@
+          (gather b c) left @@
           (c --> a) msg @@
           finish)
 
 let pa, pb, pc =
-  let g = create_g () in
-  get_sess a g, get_sess_many b g, get_sess c g
+  let g = gen_mult [1;10;1] prot in
+  get_ep a g, get_ep_list b g, get_ep c g
 
 let a : [`A] = `A
 let b : [`B] = `B
 let c : [`C] = `C
-  
+
 (* participant A *)
 let t1 : unit Lwt.t =
   let s = pa in
   let open Lwt in
-  receive c s >>= fun (`msg(x, s)) -> begin
+  receive s#role_C >>= fun (`msg(x, s)) -> begin
       Printf.printf "received: %d\n" x;
       if x = 0 then begin
-          let s = send b (fun x->x#left) (fun _ -> ()) s in
-          receive b s >>= fun (`msg(str,s)) ->
+          sendmany s#role_B#left (fun _ -> ()) >>= fun s ->
+          receive s#role_B >>= fun (`msg(str,s)) ->
           str |> List.iter (Printf.printf "A) B says: %s\n");
           close s;
           return ()
         end else begin
-          let s = send b (fun x->x#right) (fun _ -> ()) s in
-          receive b s >>= fun (`msg(xs,s)) ->
-          receive c s >>= fun (`msg(str,s)) ->
+          sendmany s#role_B#right (fun _ -> ()) >>= fun s ->
+          receive s#role_B >>= fun (`msg(xs,s)) ->
+          receive s#role_C >>= fun (`msg(str,s)) ->
           List.iteri (fun i x -> Printf.printf "A) B(%d) says: %d, C says: %s\n" i x str) xs;
           close s;
           return ()
@@ -61,18 +53,18 @@ let t1 : unit Lwt.t =
 let t2 : unit Lwt.t list =
   pb |>
   List.mapi begin fun i s ->
-  receive a s >>= begin
+  receive s#role_A >>= begin
       function
       | `left(_,s) ->
          Printf.printf "B(%d): left.\n" i;
-         let s = send c (fun x->x#right) () s in
-         let s = send a (fun x->x#msg) (Printf.sprintf "Hooray! %d" i) s in
+         send s#role_C#right () >>= fun s ->
+         send s#role_A#msg (Printf.sprintf "Hooray! %d" i) >>= fun s ->
          close s;
          Lwt.return ()
       | `right(_,s) ->
          Printf.printf "B(%d): right.\n" i;
-         let s = send a (fun x->x#msg) (1234 * (i+1)) s in
-         let s = send c (fun x->x#left) () s in
+         send s#role_A#msg (1234 * (i+1)) >>= fun s ->
+         send s#role_C#left () >>= fun s ->
          close s;
          Lwt.return ()
     end >>= fun () ->
@@ -88,11 +80,11 @@ let t3 : unit Lwt.t =
   print_endline "C: enter a number (positive or zero or negative):";
   Lwt_io.read_line Lwt_io.stdin >>= fun line ->
   let num = int_of_string line in
-  let s = send a (fun x->x#msg) num s in
-  receive b s >>= begin
+  send s#role_A#msg num >>= fun s ->
+  receive s#role_B >>= begin
       function
       | `left(_,s) -> begin
-          let s = send a (fun x->x#msg) "Hello, A!" s in
+          send s#role_A#msg "Hello, A!" >>= fun s ->
           close s;
           return ()
         end

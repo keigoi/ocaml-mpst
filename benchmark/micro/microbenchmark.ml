a89a064106b32dbc4205e0166bad0d0105bc7531
Keigo Imai <keigo.imai@gmail.com>
2019-06-29 02:41:37 +0100

    detailed iteration benchmarks

--- benchmark/micro/microbenchmark.ml
+++ benchmark/micro/microbenchmark.ml
@@ -9,8 +9,8 @@ let test_msgsize =
   Bench.Test.(
     [
      (* sanity check -- running times for these will not increase *)
-     create_indexed ~name:"msgsize/ocaml-lwt/shmem,chvec" ~args BLwtStream.test_msgsize;
-     create_indexed ~name:"msgsize/ocaml-lwt/shmem,cps" ~args (let module M = BLwtCont(LwtMVar)() in M.test_msgsize);
+     create_indexed ~name:"msgsize/OCaml-lwt/twochan" ~args (let module M = BLwtTwoChan(LwtStream)() in M.test_msgsize);
+     create_indexed ~name:"msgsize/OCaml-lwt/cps" ~args (let module M = BLwtCont(LwtMVar)() in M.test_msgsize);
 
      create_indexed ~name:"msgsize/mpst-dynamic/lwt/shmem,chvec" ~args (let module M = MakeDyn(DynCheckNano)(LwtMonad)(Shmem)() in M.test_msgsize);
      create_indexed ~name:"msgsize/mpst-static/lwt/shmem,chvec" ~args (let module M = MakeStatic(LinLwtMonad)(Shmem)() in M.test_msgsize);
@@ -56,7 +56,7 @@ let test_iteration =
         create_indexed ~name:"iter/mpst-static/lwt(stream)" ~args:last (let module M = MakeStatic(LinLwtMonad)(Shmem)() in M.test_iteration);
         (* create_indexed ~name:"iter/OCaml-lwt(bstream),cps" ~args (let module M = BLwtCont(LwtBoundedStream)() in M.test_iteration);
          * create_indexed ~name:"iter/OCaml-lwt(wake),cps" ~args (let module M = BLwtCont(LwtWait)() in M.test_iteration); *)
-        create_indexed ~name:"iter/OCaml-lwt/twochan(stream)" ~args:last BLwtStream.test_iteration;
+        create_indexed ~name:"iter/OCaml-lwt/twochan(stream)" ~args:last (let module M = BLwtTwoChan(LwtStream)() in M.test_iteration);
 
         (* Interestingly, when we use Unix pipe, event-based versions are always faster by 2x or more.
          * Also, static (monadic) versions are always faster; it seems that closures are GC'ed during i/o.
@@ -73,16 +73,18 @@ let test_iteration =
          * For Event module, differences are almost negligible (CPS is around 1 % slower), but apparently CPS allocates
          * more words than two-channel communication.
          *)
-        create_indexed ~name:"iter/OCaml-ev/twochan" ~args:last BEvent.test_iteration;
-        create_indexed ~name:"iter/OCaml-ev/cps" ~args:last BEventCont.test_iteration;
+        create_indexed ~name:"iter/OCaml-ev/twochan" ~args BEvent.test_iteration;
+        create_indexed ~name:"iter/OCaml-ev/cps" ~args BEventCont.test_iteration;
 
         (* For Lwt, CPS is slower than two-channel communication (around 5 %)
          * (It seems that Lwt_mvar is the fastest in CPS-style communication - as for Lwt version 4.2.1.
-         *  Note that MVars are 1-bounded; hence, they are not suitable for MPST implementation)
+         *  Note that MVars are 1-bounded; hence, they are not suitable for chvec MPST implementation)
          *)
-        create_indexed ~name:"iter/OCaml-lwt/twochan(stream)" ~args:last BLwtStream.test_iteration;
-        create_indexed ~name:"iter/OCaml-lwt/cps(mvar)" ~args:last (let module M = BLwtCont(LwtMVar)() in M.test_iteration);
-        create_indexed ~name:"iter/OCaml-lwt/cps(stream)" ~args:last (let module M = BLwtCont(LwtStream)() in M.test_iteration);
+        create_indexed ~name:"iter/OCaml-lwt/twochan(stream)" ~args (let module M = BLwtTwoChan(LwtStream)() in M.test_iteration);
+        create_indexed ~name:"iter/OCaml-lwt/cps(stream)" ~args (let module M = BLwtCont(LwtStream)() in M.test_iteration);
+
+        create_indexed ~name:"iter/OCaml-lwt/twochan(mvar)" ~args (let module M = BLwtTwoChan(LwtMVar)() in M.test_iteration);
+        create_indexed ~name:"iter/OCaml-lwt/cps(mvar)" ~args (let module M = BLwtCont(LwtMVar)() in M.test_iteration);
 
         (* Sanity check. running time is propotional to iteration count. *)
         (* create_indexed ~name:"iter/mpst-ref/ev" ~args:last BRefImpl.test_iteration; *)
@@ -104,7 +106,11 @@ let test_iteration =
         create_indexed ~name:"iter/mpst-dynamic/lwt" ~args:last (let module M = MakeDyn(DynCheckNano)(LwtMonad)(Shmem)() in M.test_iteration);
         create_indexed ~name:"iter/mpst-dynamic/lwt(nodyncheck)" ~args:last (let module M = MakeDyn(NoDynCheckWithClosure)(LwtMonad)(Shmem)() in M.test_iteration);
         create_indexed ~name:"iter/mpst-dynamic/lwt(nodyncheck,noclosure)" ~args:last (let module M = MakeDyn(NoDynCheck)(LwtMonad)(Shmem)() in M.test_iteration);
-        create_indexed ~name:"iter/OCaml-lwt/twochan(stream)" ~args:last BLwtStream.test_iteration;
+        create_indexed ~name:"iter/OCaml-lwt/chvec_byhand(stream)" ~args:last BLwtChannelVectorManual.test_iteration;
+        create_indexed ~name:"iter/OCaml-lwt/chvec_byhand_less_wrap(stream)" ~args:last BLwtChannelVectorManualLessWrap.test_iteration;
+        create_indexed ~name:"iter/OCaml-lwt/chvec_byhand_less_wrap2(stream)" ~args:last BLwtChannelVectorManualLessWrap2.test_iteration;
+        create_indexed ~name:"iter/OCaml-lwt/chvec_byhand_less_wrap3(stream)" ~args:last BLwtChannelVectorManualLessWrap3.test_iteration;
+        create_indexed ~name:"iter/OCaml-lwt/twochan(stream)" ~args:last (let module M = BLwtTwoChan(LwtStream)() in M.test_iteration);
 
         (* For Event-based one, the overhead for dynamic checking is negligible. Closures has some cost (less than 5 percent) *)
         create_indexed ~name:"iter/mpst-static/ev" ~args:last @@ (let module M = MakeStatic(LinDirect)(Shmem)() in M.test_iteration);
--- benchmark/micro/ocamlmicro.ml
+++ benchmark/micro/ocamlmicro.ml
@@ -107,62 +107,6 @@ module BEventCont = struct
       ()
 end
 
-module BLwtStream = struct
-  let (let/) = Lwt.bind
-
-  (* let ch1 = Lwt_stream.create ()
-   * let ch2 = Lwt_stream.create ()
-   * let receive (st,_) = Lwt_stream.next st
-   * let send (_,push) v = push (Some v); Lwt.return_unit *)
-  let ch1 = Lwt_mvar.create_empty ()
-  let ch2 = Lwt_mvar.create_empty ()
-  let receive m = Lwt_mvar.take m
-  let send m v = Lwt_mvar.put m v
-
-  let server_step () =
-    let/ arr_ = receive ch1 in
-    send ch2 ()
-
-  let test_msgsize =
-    fun i ->
-    Core.Staged.stage (fun () ->
-        Lwt.async server_step;
-        Lwt_main.run begin
-            let arr = List.assoc i big_arrays in
-            let/ () = send ch1 arr in
-            receive ch2
-          end)
-
-
-  let server_iter cnt =
-    let rec loop cnt =
-      if cnt = 0 then
-        Lwt.return_unit
-      else begin
-          let/ arr_ = receive ch1 in
-          let/ () = send ch2 () in
-          loop (cnt-1)
-        end
-    in
-    loop cnt
-
-  let test_iteration =
-    fun cnt ->
-    Core.Staged.stage (fun () ->
-        Lwt.async (fun () -> server_iter cnt);
-        Lwt_main.run begin
-            let rec loop cnt =
-              if cnt=0 then
-                Lwt.return_unit
-              else
-                let/ () = send ch1 default_payload in
-                let/ () = receive ch2 in
-                loop (cnt-1)
-            in
-            loop cnt
-          end)
-end
-
 module type LWT_CHAN = sig
   type 'a t
   val create : unit -> 'a t
@@ -192,6 +136,62 @@ module LwtWait : LWT_CHAN = struct
   let create () = Lwt.wait ()
   let send (t_,u) v = Lwt.wakeup_later u v; Lwt.return_unit
   let receive (t,u_) = t
+end
+
+module BLwtTwoChan(Chan:LWT_CHAN)() = struct
+  let (let/) = Lwt.bind
+
+  let ch1 = Chan.create ()
+  let ch2 = Chan.create ()
+  (* let receive (st,_) = Lwt_stream.next st
+   * let send (_,push) v = push (Some v); Lwt.return_unit *)
+  (* let ch1 = Lwt_mvar.create_empty ()
+   * let ch2 = Lwt_mvar.create_empty ()
+   * let receive m = Lwt_mvar.take m
+   * let send m v = Lwt_mvar.put m v *)
+
+  let server_step () =
+    let/ arr_ = Chan.receive ch1 in
+    Chan.send ch2 ()
+
+  let test_msgsize =
+    fun i ->
+    Core.Staged.stage (fun () ->
+        Lwt.async server_step;
+        Lwt_main.run begin
+            let arr = List.assoc i big_arrays in
+            let/ () = Chan.send ch1 arr in
+            Chan.receive ch2
+          end)
+
+
+  let server_iter cnt =
+    let rec loop cnt =
+      if cnt = 0 then
+        Lwt.return_unit
+      else begin
+          let/ arr_ = Chan.receive ch1 in
+          let/ () = Chan.send ch2 () in
+          loop (cnt-1)
+        end
+    in
+    loop cnt
+
+  let test_iteration =
+    fun cnt ->
+    Core.Staged.stage (fun () ->
+        Lwt.async (fun () -> server_iter cnt);
+        Lwt_main.run begin
+            let rec loop cnt =
+              if cnt=0 then
+                Lwt.return_unit
+              else
+                let/ () = Chan.send ch1 default_payload in
+                let/ () = Chan.receive ch2 in
+                loop (cnt-1)
+            in
+            loop cnt
+          end)
 end
 
 module BLwtCont(Chan:LWT_CHAN)() = struct
@@ -318,4 +318,359 @@ module Make_IPC(M:PERIPHERAL)() = struct
           loop cnt
         end
 
+end
+
+
+module BLwtChannelVectorManual = struct
+  let (let/) = Lwt.bind
+
+  let ch1 = Lwt_stream.create ()
+  let ch2 = Lwt_stream.create ()
+  let raw_receive (st,_) = Lwt_stream.next st
+  let raw_send (_,push) v = push (Some v); Lwt.return_unit
+  (* let ch1 = Lwt_mvar.create_empty ()
+   * let ch2 = Lwt_mvar.create_empty ()
+   * let receive m = Lwt_mvar.take m
+   * let send m v = Lwt_mvar.put m v *)
+
+  let create () =
+    let rec epa =
+      lazy begin
+          object method role_B =
+              object method ping =
+                  (ch1, Lazy.force epa1)
+              end
+          end
+        end
+    and epa1 =
+      lazy begin
+          object method role_B =
+              fun () -> Lwt.map (fun v -> `pong(v, Lazy.force epa)) (raw_receive ch2)
+          end
+        end
+    in
+    let rec epb =
+      lazy begin
+          object method role_A =
+              fun () -> Lwt.map (fun v -> `ping(v, Lazy.force epb1)) (raw_receive ch1)
+          end
+        end
+    and epb1 =
+      lazy begin
+          object method role_A =
+              object method pong =
+                  (ch2, Lazy.force epb)
+              end
+          end
+        end
+    in
+    Lazy.force epa, Lazy.force epb
+
+  let send (ch,cont) v =
+    let/ () = raw_send ch v in
+    Lwt.return cont
+
+  let receive ep = ep ()
+
+
+
+  let server_iter epb cnt =
+    let rec loop epb cnt =
+      if cnt = 0 then
+        Lwt.return epb
+      else begin
+          let/ `ping(_arr, epb) = receive epb#role_A in
+          let/ epb = send epb#role_A#pong () in
+          loop epb (cnt-1)
+        end
+    in
+    loop epb cnt
+
+  let test_iteration =
+    fun cnt ->
+    let epa, epb = create () in
+    Core.Staged.stage (fun () ->
+        Lwt.async (fun () -> server_iter epb cnt);
+        Lwt_main.run begin
+            let rec loop epa cnt =
+              if cnt=0 then
+                Lwt.return epa
+              else
+                let/ epa = send epa#role_B#ping default_payload in
+                let/ `pong((), epa) = receive epa#role_B in
+                loop epa (cnt-1)
+            in
+            loop epa cnt
+          end)
+end
+
+module BLwtChannelVectorManualLessWrap = struct
+  let (let/) = Lwt.bind
+
+  let ch1 = Lwt_stream.create ()
+  let ch2 = Lwt_stream.create ()
+  let raw_receive (st,_) = Lwt_stream.next st
+  let raw_send (_,push) v = push (Some v); Lwt.return_unit
+  (* let ch1 = Lwt_mvar.create_empty ()
+   * let ch2 = Lwt_mvar.create_empty ()
+   * let receive m = Lwt_mvar.take m
+   * let send m v = Lwt_mvar.put m v *)
+
+  let create () =
+    let rec epa =
+      lazy begin
+          object method role_B =
+              object method ping =
+                  (ch1, Lazy.force epa1)
+              end
+          end
+        end
+    and epa1 =
+      lazy begin
+          object method role_B =
+              (* fun () -> Lwt.map (fun v -> `pong(v, Lazy.force epa)) (raw_receive ch2) *)
+              (* fun () -> raw_receive ch2, Lazy.force epa *)
+              raw_receive ch2, Lazy.force epa
+          end
+        end
+    in
+    let rec epb =
+      lazy begin
+          object method role_A =
+              (* fun () -> Lwt.map (fun v -> `ping(v, Lazy.force epb1)) (raw_receive ch1) *)
+              (* fun () -> raw_receive ch1, Lazy.force epb1 *)
+              raw_receive ch1, Lazy.force epb1
+          end
+        end
+    and epb1 =
+      lazy begin
+          object method role_A =
+              object method pong =
+                  (ch2, Lazy.force epb)
+              end
+          end
+        end
+    in
+    (* let _: unit -> _ = Lazy.force epa1 in
+     * ignore (Lazy.force epb1); *)
+    Lazy.force epa, Lazy.force epb
+
+  let send (ch,cont) v =
+    let/ () = raw_send ch v in
+    Lwt.return cont
+
+  (* let receive ep = ep () *)
+
+  let receive ep = ep
+
+  let server_iter epb cnt =
+    let rec loop epb cnt =
+      if cnt = 0 then
+        Lwt.return epb
+      else begin
+          let inp, epb =  receive epb#role_A in
+          (* let inp, epb =  receive epb in *)
+          let/ arr_ =  inp in
+          (* let/ `ping(_arr, epb) = receive epb#role_A in *)
+          (* let/ epb = send epb () in *)
+          let/ epb = send epb#role_A#pong () in
+          loop epb (cnt-1)
+        end
+    in
+    loop epb cnt
+
+  let test_iteration =
+    fun cnt ->
+    let epa, epb = create () in
+    Core.Staged.stage (fun () ->
+        Lwt.async (fun () -> server_iter epb cnt);
+        Lwt_main.run begin
+            let rec loop epa cnt =
+              if cnt=0 then
+                Lwt.return epa
+              else
+                let/ epa = send epa#role_B#ping default_payload in
+                (* let/ epa = send epa default_payload in *)
+                (* let/ `pong((), epa) = receive epa#role_B in *)
+                (* let inp, epa = receive epa in *)
+                let inp, epa = receive epa#role_B in
+                let/ () = inp in
+                loop epa (cnt-1)
+            in
+            loop epa cnt
+          end)
+end
+
+module BLwtChannelVectorManualLessWrap2 = struct
+  let (let/) = Lwt.bind
+
+  let ch1 = Lwt_stream.create ()
+  let ch2 = Lwt_stream.create ()
+  let raw_receive (st,_) = Lwt_stream.next st
+  let raw_send (_,push) v = push (Some v); Lwt.return_unit
+  (* let ch1 = Lwt_mvar.create_empty ()
+   * let ch2 = Lwt_mvar.create_empty ()
+   * let receive m = Lwt_mvar.take m
+   * let send m v = Lwt_mvar.put m v *)
+
+  let create () =
+    let rec epa =
+      lazy begin
+                  (ch1, Lazy.force epa1)
+        end
+    and epa1 =
+      lazy begin
+              fun () -> Lwt.map (fun v -> `pong(v, Lazy.force epa)) (raw_receive ch2)
+              (* fun () -> raw_receive ch2, Lazy.force epa *)
+              (* raw_receive ch2, Lazy.force epa *)
+        end
+    in
+    let rec epb =
+      lazy begin
+              fun () -> Lwt.map (fun v -> `ping(v, Lazy.force epb1)) (raw_receive ch1)
+              (* fun () -> raw_receive ch1, Lazy.force epb1 *)
+              (* raw_receive ch1, Lazy.force epb1 *)
+        end
+    and epb1 =
+      lazy begin
+                  (ch2, Lazy.force epb)
+        end
+    in
+    (* let _: unit -> _ = Lazy.force epa1 in
+     * ignore (Lazy.force epb1); *)
+    Lazy.force epa, Lazy.force epb
+
+  let send (ch,cont) v =
+    let/ () = raw_send ch v in
+    Lwt.return cont
+
+  let receive ep = ep ()
+
+  (* let receive ep = ep *)
+
+  let server_iter epb cnt =
+    let rec loop epb cnt =
+      if cnt = 0 then
+        Lwt.return epb
+      else begin
+          (* let inp, epb =  receive epb#role_A in *)
+          (* let inp, epb =  receive epb in *)
+          (* let/ arr_ =  inp in *)
+          (* let/ `ping(_arr, epb) = receive epb#role_A in *)
+          let/ `ping(_arr, epb) = receive epb in
+          let/ epb = send epb () in
+          (* let/ epb = send epb#role_A#pong () in *)
+          loop epb (cnt-1)
+        end
+    in
+    loop epb cnt
+
+  let test_iteration =
+    fun cnt ->
+    let epa, epb = create () in
+    Core.Staged.stage (fun () ->
+        Lwt.async (fun () -> server_iter epb cnt);
+        Lwt_main.run begin
+            let rec loop epa cnt =
+              if cnt=0 then
+                Lwt.return epa
+              else
+                (* let/ epa = send epa#role_B#ping default_payload in *)
+                let/ epa = send epa default_payload in
+                let/ `pong((), epa) = receive epa in
+                (* let/ `pong((), epa) = receive epa#role_B in *)
+                (* let inp, epa = receive epa in *)
+                (* let inp, epa = receive epa#role_B in *)
+                (* let/ () = inp in *)
+                loop epa (cnt-1)
+            in
+            loop epa cnt
+          end)
+end
+
+module BLwtChannelVectorManualLessWrap3 = struct
+  let (let/) = Lwt.bind
+
+  let ch1 = Lwt_stream.create ()
+  let ch2 = Lwt_stream.create ()
+  let raw_receive (st,_) = Lwt_stream.next st
+  let raw_send (_,push) v = push (Some v); Lwt.return_unit
+  (* let ch1 = Lwt_mvar.create_empty ()
+   * let ch2 = Lwt_mvar.create_empty ()
+   * let receive m = Lwt_mvar.take m
+   * let send m v = Lwt_mvar.put m v *)
+
+  let create () =
+    let rec epa =
+      lazy begin
+                  (ch1, Lazy.force epa1)
+        end
+    and epa1 =
+      lazy begin
+              (* fun () -> Lwt.map (fun v -> `pong(v, Lazy.force epa)) (raw_receive ch2) *)
+              fun () -> raw_receive ch2, Lazy.force epa
+              (* raw_receive ch2, Lazy.force epa *)
+        end
+    in
+    let rec epb =
+      lazy begin
+              (* fun () -> Lwt.map (fun v -> `ping(v, Lazy.force epb1)) (raw_receive ch1) *)
+              fun () -> raw_receive ch1, Lazy.force epb1
+              (* raw_receive ch1, Lazy.force epb1 *)
+        end
+    and epb1 =
+      lazy begin
+                  (ch2, Lazy.force epb)
+        end
+    in
+    (* let _: unit -> _ = Lazy.force epa1 in
+     * ignore (Lazy.force epb1); *)
+    Lazy.force epa, Lazy.force epb
+
+  let send (ch,cont) v =
+    let/ () = raw_send ch v in
+    Lwt.return cont
+
+  let receive ep = ep ()
+
+  (* let receive ep = ep *)
+
+  let server_iter epb cnt =
+    let rec loop epb cnt =
+      if cnt = 0 then
+        Lwt.return epb
+      else begin
+          (* let inp, epb =  receive epb#role_A in *)
+          let inp, epb =  receive epb in
+          let/ arr_ =  inp in
+          (* let/ `ping(_arr, epb) = receive epb#role_A in *)
+          (* let/ `ping(_arr, epb) = receive epb in *)
+          let/ epb = send epb () in
+          (* let/ epb = send epb#role_A#pong () in *)
+          loop epb (cnt-1)
+        end
+    in
+    loop epb cnt
+
+  let test_iteration =
+    fun cnt ->
+    let epa, epb = create () in
+    Core.Staged.stage (fun () ->
+        Lwt.async (fun () -> server_iter epb cnt);
+        Lwt_main.run begin
+            let rec loop epa cnt =
+              if cnt=0 then
+                Lwt.return epa
+              else
+                (* let/ epa = send epa#role_B#ping default_payload in *)
+                let/ epa = send epa default_payload in
+                (* let/ `pong((), epa) = receive epa in *)
+                (* let/ `pong((), epa) = receive epa#role_B in *)
+                let inp, epa = receive epa in
+                (* let inp, epa = receive epa#role_B in *)
+                let/ () = inp in
+                loop epa (cnt-1)
+            in
+            loop epa cnt
+          end)
 end

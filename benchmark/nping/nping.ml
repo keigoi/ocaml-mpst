abae8054800951ad029c33a1a3590bbbe2db927c
Keigo Imai <keigo.imai@gmail.com>
2019-10-20 05:45:38 +0900

    benchmark: N-Ping: detailed analysis

--- benchmark/nping/nping.ml
+++ benchmark/nping/nping.ml
@@ -2,13 +2,13 @@ open Core_bench.Bench
 open Bench_util.Util
 open Npingbody
 
-(* let run f = Core.Staged.unstage (f (List.nth array_sizes 0)) *)
+let nping_num = [750; 500; 100; 20]
 
-(* let nping_num = [1; 100; 10000] *)
-(* let nping_num = [100; 500; 2000; 4000] *)
-let nping_num = [4000; 2000; 500; 100]
-(* let nping_num = [1] *)
-
+module FastEP = Mpst.Endpoints.Make(Mpst.Lin.NoCheck)
+module NoReuseMutexEP = Mpst.Endpoints.Make(Mpst.Lin.MakeDynCheckClosure(Mpst.LinFlag.PosixMutexFlag))
+module NanoMutexEP = Mpst.Endpoints.Make(Mpst.Lin.MakeDynCheck(Bench_util.Dyncheck_nanomutex.NanoMutexFlag))
+module NoReuseNanoMutexEP = Mpst.Endpoints.Make(Mpst.Lin.MakeDynCheckClosure(Bench_util.Dyncheck_nanomutex.NanoMutexFlag))
+              
 let run ~name t =
   Test.create_indexed ~args:nping_num ~name t
 
@@ -22,7 +22,13 @@ let lwt_mpst_static =
   run ~name:"lwt-mpst_static" (let module M = MakeStatic(LinLwtMonad)(Shmem)() in M.runtest)
 
 let lwt_mpst_static_untyped =
-  run ~name:"lwt-mpst_static" (let module M = MakeStatic(LinLwtMonad)(Untyped)() in M.runtest)
+  run ~name:"lwt-mpst_static_untyped" (let module M = MakeStatic(LinLwtMonad)(Untyped)() in M.runtest)
+
+let lwt_mpst_faster =
+  run ~name:"lwt-mpst_faster" (let module M = MakeDyn(FastEP)(LwtMonad)(Shmem)() in M.runtest)
+
+let lwt_mpst_faster_untyped =
+  run ~name:"lwt-mpst_faster_untyped" (let module M = MakeDyn(FastEP)(LwtMonad)(Untyped)() in M.runtest)
 
 let ev_mpst_dynamic =
   run ~name:"ev-mpst_dynamic" (let module M = MakeDyn (Mpst.EP)(Direct)(Shmem)() in M.runtest)
@@ -30,14 +36,35 @@ let ev_mpst_dynamic =
 let ev_mpst_dynamic_untyped =
   run ~name:"ev-mpst_dynamic_untyped" (let module M = MakeDyn (Mpst.EP)(Direct)(Untyped)() in M.runtest)
 
-let ev_mpst_static =
-  run ~name:"ev-mpst_static" @@ (let module M = MakeStatic(LinDirect)(Shmem)() in M.runtest)
+let ev_mpst_noreuse =
+  run ~name:"ev-mpst_noreuse" (let module M = MakeDyn (NoReuseMutexEP)(Direct)(Shmem)() in M.runtest)
+
+let ev_mpst_noreuse_untyped =
+  run ~name:"ev-mpst_noreuse_untyped" (let module M = MakeDyn (NoReuseMutexEP)(Direct)(Untyped)() in M.runtest)
+
+let ev_mpst_nanomutex =
+  run ~name:"ev-mpst_nano" (let module M = MakeDyn (NanoMutexEP)(Direct)(Shmem)() in M.runtest)
+
+let ev_mpst_nanomutex_untyped =
+  run ~name:"ev-mpst_nano_untyped" (let module M = MakeDyn (NanoMutexEP)(Direct)(Untyped)() in M.runtest)
+
+let ev_mpst_nanomutex_noreuse =
+  run ~name:"ev-mpst_nano-noreuse" (let module M = MakeDyn (NoReuseNanoMutexEP)(Direct)(Shmem)() in M.runtest)
+
+let ev_mpst_nanomutex_noreuse_untyped =
+  run ~name:"ev-mpst_nano-noreuse_untyped" (let module M = MakeDyn (NoReuseNanoMutexEP)(Direct)(Untyped)() in M.runtest)
 
 let ev_mpst_static =
   run ~name:"ev-mpst_static" @@ (let module M = MakeStatic(LinDirect)(Shmem)() in M.runtest)
 
 let ev_mpst_static_untyped =
-  run ~name:"ev-mpst_static" @@ (let module M = MakeStatic(LinDirect)(Untyped)() in M.runtest)
+  run ~name:"ev-mpst_static_untyped" @@ (let module M = MakeStatic(LinDirect)(Untyped)() in M.runtest)
+
+let ev_mpst_faster =
+  run ~name:"ev-mpst_faster" (let module M = MakeDyn (FastEP)(Direct)(Shmem)() in M.runtest)
+
+let ev_mpst_faster_untyped =
+  run ~name:"ev-mpst_faster_untyped" (let module M = MakeDyn (FastEP)(Direct)(Untyped)() in M.runtest)
 
 let test_lwt =
   Core_bench.Bench.Test.(
@@ -46,6 +73,8 @@ let test_lwt =
         lwt_mpst_dynamic_untyped;
         lwt_mpst_static;
         lwt_mpst_static_untyped;
+        lwt_mpst_faster;
+        lwt_mpst_faster_untyped;
       ]
   )
 
@@ -55,9 +84,17 @@ let test_ev =
   Bench.Test.(
       [
         ev_mpst_dynamic;
-        ev_mpst_dynamic_untyped;
+        ev_mpst_noreuse;
+        ev_mpst_nanomutex;
+        ev_mpst_nanomutex_noreuse;
         ev_mpst_static;
+        ev_mpst_faster;
+        ev_mpst_dynamic_untyped;
+        ev_mpst_noreuse_untyped;
+        ev_mpst_nanomutex_untyped;
+        ev_mpst_nanomutex_noreuse_untyped;
         ev_mpst_static_untyped;
+        ev_mpst_faster_untyped;
       ])
 
 let test_ipc =
@@ -66,6 +103,7 @@ let test_ipc =
   Bench.Test.(
       [
         run ~name:"ipc-mpst_dynamic" (let module M = MakeDyn(Mpst.EP)(Direct)(IPC)() in M.runtest);
+        run ~name:"ipc-mpst_faster" (let module M = MakeDyn(FastEP)(Direct)(IPC)() in M.runtest);
         run ~name:"ipc-mpst_static" (let module M = MakeStatic(LinDirect)(IPC)() in M.runtest);
       ]
   )
@@ -78,4 +116,5 @@ let () =
     Core_bench.Bench.make_command
       (* test_ev *)
       (* test_lwt *)
+      (* test_ipc *)
       test_iteration

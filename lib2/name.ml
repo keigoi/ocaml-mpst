fc47723a2293ef55c386e6d1bc01b47841871edd
Keigo Imai <keigo.imai@gmail.com>
2020-04-14 07:44:52 +0900

    wip

--- lib2/name.ml
+++ lib2/name.ml
@@ -1,135 +1,225 @@
-type 't st = {
-    stream : 't Lwt_stream_opt.t;
-    mutable out_refs : 't out_ref list;
-    mutable inp_refs : 't inp_ref list;
-  }
-and 't st_gather = {
-    stream_g : ('t Lwt_stream_opt.t * 't out_ref) list;
-    mutable inp_refs_g : 't gather_ref list;
-  }
-and _ out_ref =
-  (* reference to the wrappeed channel and wraping function *)
-  |  OutRef : 'u out * ('u -> 't) -> 't out_ref
+
+type 't st = 't Lwt_stream_opt.t
+let create_st () = Lwt_stream_opt.create ()
+
+type ('t,'u) out0 =
+  {o_st: 'u st;
+   o_wrap: ('t -> 'u);
+   o_siblings: 't out list;
+   o_inp_refs: 'u inp_ref list}  
 and 't out_ =
   (* triple of original stream, wraping function and siblings (merged wrappers) of same type *)
-  Out : 'u st * ('t -> 'u) * 't out list -> 't out_
-and 't out = 't out_ ref
-and _ inp_ref =
-  |  InpRef : 'u inp * ('t -> 'u option) -> 't inp_ref
+    Out : ('t, 'u) out0 -> 't out_
+and 't out =
+  't out_ ref
+and ('u,'t) inp0 =
+  {i_st: 't st;
+   i_wrap: ('t -> 'u option);
+   i_siblings: 'u inp list;
+   i_out_refs: 't out_ref list}  
 and _ inp_ =
-  | Inp : 't st * ('t -> 'u option) * 'u inp list -> 'u inp_
-and 'u inp = 'u inp_ ref
-and _ gather_ref =
-  |  GatherRef : 'u gather * ('t list -> 'u option) -> 't gather_ref
-and _ gather_ =
-  | Gather : 't st list * ('t list  -> 'u option) * 'u inp list -> 'u gather_
-and 'u gather = 'u gather_ ref
+    Inp : ('u, 't) inp0 -> 'u inp_
+and 'u inp =
+  'u inp_ ref
+and _ out_ref =
+    OutRef : 'u out * ('u -> 't) -> 't out_ref
+and _ inp_ref =
+    InpRef : 't inp * ('u -> 't option) -> 'u inp_ref
+
+type _ gather_ =
+    Gather : 'u inp list * ('v list -> 'w) -> 'w gather_
+and 'w gather =
+  'w gather_ ref
+
 
 (* auxiliary data types for "internal protocol" *)
 type ('a, 'b) either = Left of 'a | Right of 'b
 
+let id x = x
 let in_left x = Left(x)
 let in_right x = Right(x)
 let fst_some (x,_) = Some x
 let snd_some (_,x) = Some x
 
-let join_option f m0 =
+let bind_option f m0 =
   match m0 with
   | Some x -> f x
   | None -> None
 
-let create_raw () =
-  let t = {
-      stream = Lwt_stream_opt.create ();
-      out_refs = [];
-      inp_refs = [];
-    }
-  in
-  t
+let map_option f m0 =
+  match m0 with
+  | Some x -> Some (f x)
+  | None -> None
 
-let update_out_ref : type t u. u st -> (t -> u) -> t out_ref -> u out_ref =
-  fun new_st f -> function
-  | OutRef({contents=Out(_,_,siblings)} as r, outf) ->
-     let outf' = fun x -> f (outf x) in
-     r := Out(new_st, outf', siblings);
-     OutRef(r,outf')
+let update_out
+    (type t u) ((st : u st), (wrap : t -> u), (inp_refs : u inp_ref list))
+  : t out_ref -> u out_ref =
+  function
+  | OutRef({contents=Out({o_siblings; _})} as out, wrap_orig) ->
+    let wrap_new x = wrap (wrap_orig x) in
+    (* update output refs *)
+    out := Out {o_st = st;
+                o_wrap = wrap_new;
+                o_inp_refs = inp_refs;
+                o_siblings;
+               };
+    OutRef(out, wrap_new)
 
-let update_inp_ref : type t u. u st -> (u -> t option) -> t inp_ref -> u inp_ref =
-  fun st g -> function
-  | (InpRef({contents=Inp(_,_,siblings)} as r,in_)) ->
-     let g = fun x -> join_option in_ (g x) in
-     r := Inp(st,g,siblings);
-     InpRef(r,g)
-
-(** Merge two streams into one, via input endpoint  *)
+(** Merge two input endpoints into one  *)
 let merge_inp : type t. t inp -> t inp -> t inp = fun inpL inpR ->
   match inpL, inpR with
   | _ when inpL==inpR ->
      inpL
-  | {contents=Inp(st_l, f_l, siblings_l)},
-    {contents=Inp(st_r, f_r, siblings_r)} ->
-     let st : (_,_) either st = create_raw () in
-     let f_lr = function Left x -> f_l x | Right x -> f_r x in
-     let siblings = List.append siblings_l siblings_r in
-     let inp = Inp(st, f_lr, siblings) in
-     List.iter (fun r0 -> r0 := inp) siblings;
-     st.out_refs <-
-       List.map (update_out_ref st in_left) st_l.out_refs @
-       List.map (update_out_ref st in_right) st_r.out_refs;
-     st.inp_refs <-
-       List.map (fun inp -> InpRef(inp, f_lr)) siblings;
-     inpL
+  | {contents=Inp(inpL0)},
+    {contents=Inp(inpR0)} ->
+    let st : (_,_) either st = create_st () in
+    (* new wrap function *)
+    let wrap = function Left x -> inpL0.i_wrap x | Right x -> inpR0.i_wrap x in
+    let siblings = List.append inpL0.i_siblings inpR0.i_siblings in
+    let inp_refs = List.map (fun r0 -> InpRef(r0, wrap)) siblings in
+    let out_refs =
+      (* contents are updated later *)
+      List.map (update_out (st, in_left, inp_refs)) inpL0.i_out_refs @
+      List.map (update_out (st, in_right, inp_refs)) inpR0.i_out_refs
+    in
+    let new_inp =
+      Inp {i_st = st;
+           i_wrap = wrap;
+           i_siblings = siblings;
+           i_out_refs = out_refs}
+    in
+    (* update inputs to the merged ones *)
+    List.iter (fun r0 -> r0 := new_inp) siblings;
+    inpL
+
+(** assign to the input the given stream and wrapper, via given inp_ref *)
+let update_inp
+    (type t u) ((st : u st), (wrap : u -> t option), (out_refs : u out_ref list))
+  : t inp_ref -> u inp_ref =
+  function
+  | InpRef({contents=Inp({i_siblings; _})} as r, wrap_orig) ->
+    let wrap_new x = bind_option wrap_orig (wrap x) in
+    r := Inp({i_st = st;
+              i_wrap = wrap_new;
+              i_out_refs = out_refs;
+              i_siblings});
+    InpRef(r, wrap_new)
 
 (** Merge two streams into one via output endpoint  *)
 let merge_out : type t. t out -> t out -> t out = fun outL outR ->
-  match outR, outR with
-  | _ when outL==outL ->
+  match outL, outR with
+  | _ when outL==outR ->
      outL
-  | {contents=Out(st_l, f_l, siblings_l)},
-    {contents=Out(st_r, f_r, siblings_r)} ->
+  | {contents=Out(outL0)}, {contents=Out(outR0)} ->
+    let st : (_ * _) st = create_st () in
     (* make a new pair stream *) 
-    let st : (_ * _) st = create_raw () in
-    (* new output wrapper *)
-    let f_lr = fun x -> (f_l x, f_r x) in
-    let siblings = List.append siblings_l siblings_r in
-    (* construct the new out referring the new stream *)
-    let new_out = Out(st, f_lr, siblings) in    
-    (* udpate siblings to the new out (including outL and outR) *)
-    List.iter (fun r0 -> r0 := new_out) siblings;
-    (* wiring new out/inp refs *)
-    st.out_refs <-
-      List.map (fun out -> OutRef(out, f_lr)) siblings;
-    st.inp_refs <-
-      List.map (update_inp_ref st fst_some) st_l.inp_refs @
-      List.map (update_inp_ref st snd_some) st_r.inp_refs;
-    st_l.inp_refs <- [];
-    st_l.out_refs <- [];
-    st_r.inp_refs <- [];
-    st_r.out_refs <- [];
+    let outs = List.append outL0.o_siblings outR0.o_siblings in
+    let wrap x = (outL0.o_wrap x, outR0.o_wrap x) in
+    let out_refs = List.map (fun out -> OutRef(out, wrap)) outs in
+    let inp_refs =
+      List.map (update_inp (st, fst_some, out_refs)) outL0.o_inp_refs @
+      List.map (update_inp (st, snd_some, out_refs)) outR0.o_inp_refs
+    in
+    let new_out =
+      Out {o_st = st;
+           o_wrap = wrap;
+           o_siblings = outs;
+           o_inp_refs = inp_refs
+          }
+    in
+    (* let merged outputs point to the new one *)
+    List.iter (fun out -> out := new_out) outs;
     outL
 
-let wrap_inp st f =
-  let f x = Some (f x) in
-  let rec inp = {contents=Inp(st,f,[inp])} in
-  st.inp_refs <- [InpRef(inp, f)];
-  inp
+(** Merge two input endpoints into one  *)
+let merge_inp : type t. t inp -> t inp -> t inp = fun inpL inpR ->
+  match inpL, inpR with
+  | _ when inpL==inpR ->
+     inpL
+  | {contents=Inp(inpL0)},
+    {contents=Inp(inpR0)} ->
+    let st : (_,_) either st = create_st () in
+    (* new wrap function *)
+    let wrap = function Left x -> inpL0.i_wrap x | Right x -> inpR0.i_wrap x in
+    let siblings = List.append inpL0.i_siblings inpR0.i_siblings in
+    let inp_refs = List.map (fun r0 -> InpRef(r0, wrap)) siblings in
+    let out_refs =
+      (* contents are updated later *)
+      List.map (update_out (st, in_left, inp_refs)) inpL0.i_out_refs @
+      List.map (update_out (st, in_right, inp_refs)) inpR0.i_out_refs
+    in
+    let new_inp =
+      Inp {i_st = st;
+           i_wrap = wrap;
+           i_siblings = siblings;
+           i_out_refs = out_refs}
+    in
+    (* update inputs to the merged ones *)
+    List.iter (fun r0 -> r0 := new_inp) siblings;
+    inpL
 
-let wrap_out st f =
-  let rec out = {contents=Out(st,f,[out])} in
-  st.out_refs <- [OutRef(out, f)];
-  out
+let hetero_merge_inp : type t u. t inp -> u inp -> (t,u) either inp =
+  fun {contents=Inp(inpL0)} {contents=Inp(inpR0)} ->
+    let st : (_,_) either st = create_st () in
+    let wrap = function
+      | Left x -> map_option (fun x -> Left x) @@ inpL0.i_wrap x
+      | Right x -> map_option (fun x -> Right x) @@ inpR0.i_wrap x
+    in
+    let inp = ref @@ (*dummy*)Inp{i_st = st; i_wrap = wrap; i_siblings = []; i_out_refs = []} in
+    let inp_ref = InpRef(inp, wrap) in
+    let out_refs =
+      (* contents are updated later *)
+      List.map (update_out (st, in_left, [inp_ref])) inpL0.i_out_refs @
+      List.map (update_out (st, in_right, [inp_ref])) inpR0.i_out_refs
+    in
+    (* wiring it *)
+    let inp_ = Inp {i_st = st; i_wrap = wrap; i_siblings = [inp]; i_out_refs = out_refs} in
+    inp := inp_;
+    inp
+
+let merge_gather : type t. t gather -> t gather -> t gather = fun gl gr ->
+  match gl, gr with
+  | {contents=Gather(inpsL, wrapL)},
+    {contents=Gather(inpsR, wrapR)} ->
+    let inps = List.map2 hetero_merge_inp inpsL inpsR in
+    let wrap_left = function
+      | Left x -> x
+      | Right _ -> failwith "gather: reception failure: Right"
+    and wrap_right = function
+      | Right x -> x
+      | Left _ -> failwith "gather: reception failure: Left"
+    in
+    let wrap = function
+      | Left x::xs -> wrapL @@ x::List.map wrap_left xs
+      | Right x::xs -> wrapR @@ x::List.map wrap_right xs
+      | [] -> failwith "gather: reception failure: empty"
+    in
+    let g = Gather(inps, wrap) in
+    gl := g;
+    gr := g;
+    gl
 
 let create f =
-  let st = create_raw () in
-  wrap_out st (fun x -> x), wrap_inp st f
+  let st = create_st () in
+  let wrap x = Some (f x) in
+  let rec out = {contents=Out({o_st = st; o_wrap = id; o_siblings=[out]; o_inp_refs = [InpRef(inp, wrap)]})}
+  and inp = {contents=Inp({i_st = st; i_wrap = wrap; i_siblings=[inp]; i_out_refs = [OutRef(out, id)]})}
+  in
+  out, inp
 
 let send o v =
   match o with
-  | {contents=Out(st,f,_)} ->
-     (* FIXME replace List.nth with lazy?? *)
-     Lwt_stream_opt.send st.stream (f v)
+  | {contents=Out({o_st; o_wrap; _})} ->
+     Lwt_stream_opt.send o_st (o_wrap v)
 
 let receive = function
-  | {contents=Inp(st,f,_)} ->
-      (* FIXME replace List.nth with lazy?? *)
-     Lwt_stream_opt.receive_wrap ~f st.stream
+  | {contents=Inp({i_st; i_wrap; _})} ->
+    Lwt_stream_opt.receive_wrap ~f:i_wrap i_st
+
+let create_gather cnt (f : 'v list -> 't) =
+  let outs, inps = List.split @@ List.init cnt (fun _ -> create id) in
+  outs, ref (Gather(inps, f))
+
+let create_scatter cnt f =
+  let outs, inps = List.split @@ List.init cnt (fun _ -> create f) in
+  outs, inps

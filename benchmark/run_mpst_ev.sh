84feb84d667457d92bfb725730ef824489c7f665
Keigo Imai <keigo.imai@gmail.com>
2020-04-17 17:22:17 +0900

    run

---
+++ benchmark/run_mpst_ev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+if [ $# -eq 0 ]
+then
+    QUOTA=10s
+else
+	QUOTA=$1
+fi
+echo running benchmark with quota of $QUOTA seconds.
+set -e
+set -v
+
+dune build @mpst/all
+
+../_build/default/benchmark/mpst/pingpong/pingpong.exe +time cycles alloc gc percentage speedup samples -quota $QUOTA -sexp >mpst/pingpong/results/table_sexp_ev.txt
+../_build/default/benchmark/mpst/nping/nping.exe +time cycles alloc gc percentage speedup samples -quota $QUOTA -sexp >mpst/nping/results/table_sexp_ev.txt
+../_build/default/benchmark/mpst/chameleons/chameleons.exe +time cycles alloc gc percentage speedup samples -quota $QUOTA -sexp >mpst/chameleons/results/table_sexp_ev.txt

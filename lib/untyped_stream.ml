a4b88e674710026482f24b1e1e7fa9711074cf40
Keigo Imai <keigo.imai@gmail.com>
2020-04-17 12:37:56 +0900

    fix

--- lib/untyped_stream.ml
+++ lib/untyped_stream.ml
@@ -1,33 +1,33 @@
 open Base
 
-  type 'v stream = 'v Stream_opt.t
-  type 'v t = {me:'v stream; othr:'v stream}
+type 'v stream = 'v Name.out * 'v Name.inp
+type 'v t = {me:'v stream; othr:'v stream}
 
 module Make(X:sig type 'a t and 'a u val fresh : 'a t -> 'a u end) = struct
   module U = Untyped.Make(X)
   
   let create () =
-    {me=Stream_opt.create (); othr=Stream_opt.create ()}
+    {me=Name.create (fun x -> x); othr=Name.create (fun x -> x)}
   
   let flip {me=othr;othr=me} =
     {me;othr}
   
   let out_untyped ch label : 'v U.out = fun v ->
     let tag = Untyped.make_tag label.var in
-    Stream_opt.send ch.othr (tag, Obj.repr v)
+    Name.send (fst ch.othr) (tag, Obj.repr v)
   
   let wrap var (v, cont) =
     var (Obj.obj v, cont)
   
   let inp_untyped ch label cont : 'var U.inp =
     let tag = Untyped.make_tag label.var in
-    (fun () -> Stream_opt.receive ch.me), [(tag, U.Wrapper (cont, wrap label.var))]
+    (fun () -> Name.receive (snd ch.me)), [(tag, U.Wrapper (cont, wrap label.var))]
   
   let wrap_list var (vs, cont) =
     var (List.map Obj.obj vs, cont)
   
   let inplist_untyped chs label cont : 'var U.inplist =
     let tag = Untyped.make_tag label.var in
-    List.map (fun ch -> (fun () -> Stream_opt.receive ch.me)) chs,
+    List.map (fun ch -> (fun () -> Name.receive (snd ch.me))) chs,
     [(tag, U.Wrapper (cont, wrap_list label.var))]
 end

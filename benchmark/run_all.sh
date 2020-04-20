6be81406b4b476372520edb6d888cd9a73510ec6
Keigo Imai <keigo.imai@gmail.com>
2020-04-20 10:42:10 +0900

    benchmark script

--- benchmark/run_all.sh
+++ benchmark/run_all.sh
@@ -18,29 +18,52 @@ set -e
 
 QUOTA=$1
 
-DIR=`mktemp -d -p results`
+DIR=`mktemp -d -p .`
 cd $DIR
 
 echo Saving raw results to $DIR ..
 
 set -v
 
+# extend file open limit
+ulimit -n 65535
+
 opam switch ocaml-mpst-ev
 eval `opam env`
 dune clean
 
+mkdir ev_bare_pingpong; pushd ev_bare_pingpong
 dune exec ../../ev/ev_pingpong.exe -- +time cycles alloc gc percentage speedup samples -save -quota $QUOTA
+dune exec ../../ev/ev_pingpong.exe -- -load $(echo $(ls *s.txt) | sed 's/ / -load /g') -sexp >../../results/ev_bare_pingpong.txt
+popd
+
+mkdir ev_pingpong; pushd ev_pingpong
 dune exec ../../mpst/pingpong/pingpong.exe -- +time cycles alloc gc percentage speedup samples -save -quota $QUOTA
+dune exec ../../mpst/pingpong/pingpong.exe -- -load $(echo $(ls *s.txt) | sed 's/ / -load /g') -sexp >../../results/ev_pingpong.txt
+popd
+
 
 opam switch ocaml-mpst-lwt
 eval `opam env`
 dune clean
 
+mkdir -p lwt_bare_pingpong; pushd lwt_bare_pingpong
 dune exec ../../lwt/lwt_pingpong.exe -- +time cycles alloc gc percentage speedup samples -save -quota $QUOTA
+dune exec ../../lwt/lwt_pingpong.exe -- -load $(echo $(ls *s.txt) | sed 's/ / -load /g') -sexp >../../results/lwt_bare_pingpong.txt
+popd
+
+mkdir -p lwt_pingpong; pushd lwt_pingpong
 dune exec ../../mpst/pingpong/pingpong.exe -- +time cycles alloc gc percentage speedup samples -save -quota $QUOTA
+dune exec ../../mpst/pingpong/pingpong.exe -- -load $(echo $(ls *s.txt) | sed 's/ / -load /g') -sexp >../../results/lwt_pingpong.txt
+popd
+
+mkdir -p lwt_nping; pushd lwt_nping
 dune exec ../../mpst/nping/nping.exe -- +time cycles alloc gc percentage speedup samples -save -quota $QUOTA
+dune exec ../../mpst/nping/nping.exe -- -load $(echo $(ls *s.txt) | sed 's/ / -load /g') -sexp >../../results/lwt_nping.txt
+popd
+
+mkdir -p lwt_chameleons; pushd lwt_chameleons
 dune exec ../../mpst/chameleons/chameleons.exe -- +time cycles alloc gc percentage speedup samples -save -quota $QUOTA
+dune exec ../../mpst/chameleons/chameleons.exe -- -load $(echo $(ls *s.txt) | sed 's/ / -load /g') -sexp >../../results/lwt_chameleons.txt
+popd
 
-echo Saved raw results to $DIR .
-
-dune exec ../../mpst/pingpong/pingpong.exe -- -load $(echo $(ls *s.txt) | sed 's/ / -load /g') -sexp >../table_sexp.txt

3d8702e0dd424316d97f7cf3df40a0a022f389ce
Keigo Imai <keigo.imai@gmail.com>
2022-06-17 09:31:22 +0900

    minor

--- lib/multicast/actionScatter.ml
+++ lib/multicast/actionScatter.ml
@@ -3,10 +3,8 @@ module Context = State.Context
 
 type tag = int
 
-type (_, _) scatter_val_ =
-  | Scatter :
-      string * 'v DynChan.name list * 's LinState.t lazy_t
-      -> ('v, 's) scatter_val_
+type ('v, 's) scatter_val_ =
+  string * 'v DynChan.name list * 's LinState.t lazy_t
 
 type ('v, 's) scatter_val = ('v, 's) scatter_val_ Lin.lin
 type 'a scatter = (tag, 'a) scatter_val
@@ -15,23 +13,22 @@ let scatter_op0 (type v b) : (v, b) scatter_val_ State.op =
   let module M = struct
     type nonrec a = (v, b) scatter_val_
 
-    let determinise ctx (Scatter (tag, name, cont)) =
-      Scatter (tag, name, lazy (PowState.determinise ctx (Lazy.force cont)))
+    let determinise ctx (tag, name, cont) =
+      (tag, name, lazy (PowState.determinise ctx (Lazy.force cont)))
 
-    let merge ctx (Scatter (tag1, names1, cont1))
-        (Scatter (tag2, names2, cont2)) =
+    let merge ctx (tag1, names1, cont1) (tag2, names2, cont2) =
       assert (tag1 = tag2);
       List.iter2 DynChan.unify names1 names2;
-      Scatter
-        ( tag1,
-          names1,
-          lazy (PowState.merge ctx (Lazy.force cont1) (Lazy.force cont2)) )
 
-    let force ctx (Scatter (_, names, cont)) =
+      ( tag1,
+        names1,
+        lazy (PowState.merge ctx (Lazy.force cont1) (Lazy.force cont2)) )
+
+    let force ctx (_, names, cont) =
       ignore (List.map DynChan.finalise names);
       PowState.force ctx (Lazy.force cont)
 
-    let to_string ctx (Scatter (_, _, cont)) =
+    let to_string ctx (_, _, cont) =
       "."
       ^
       if Lazy.is_val cont then
@@ -55,7 +52,7 @@ let make_scatter role lab name s =
   let st =
     Lin.map_gen (fun t -> role.make_obj @@ lab.make_obj t)
     @@ Lin.declare
-    @@ Scatter (lab.method_name, name, Lazy.from_val s)
+    @@ (lab.method_name, name, Lazy.from_val s)
   in
   PowState.make (scatter_op role lab) st
 
@@ -63,19 +60,19 @@ let make_scatter_val role name s =
   let st =
     (role.make_obj |> Lin.map_gen)
     @@ Lin.declare
-    @@ Scatter ("_out" (*FIXME*), name, Lazy.from_val s)
+    @@ ("_out" (*FIXME*), name, Lazy.from_val s)
   in
   PowState.make (scatter_val_op role) st
 
 let scatter s =
-  let (Scatter (labname, names, cont)) = Lin.use s in
+  let labname, names, cont = Lin.use s in
   let tag = Btype.hash_variant labname in
   let cont = PowState.ensure_determinised (Lazy.force cont) in
   List.iter (fun name -> DynChan.send (DynChan.finalise name) tag) names;
   Lin.fresh cont
 
 let scatter_val s vs =
-  let (Scatter (_, names, cont)) = Lin.use s in
+  let _, names, cont = Lin.use s in
   let cont = PowState.ensure_determinised (Lazy.force cont) in
   List.iter2 (fun name v -> DynChan.send (DynChan.finalise name) v) names vs;
   Lin.fresh cont
--- lib/unicast/actionOut.ml
+++ lib/unicast/actionOut.ml
@@ -2,10 +2,7 @@ open Rows
 module Context = State.Context
 
 type tag = int
-
-type ('v, 's) out_ =
-  | Out : string * 'v DynChan.name * 's LinState.t lazy_t -> ('v, 's) out_
-
+type ('v, 's) out_ = string * 'v DynChan.name * 's LinState.t lazy_t
 type ('v, 'a) out = ('v, 'a) out_ Lin.lin
 type 's select = (tag, 's) out
 
@@ -13,22 +10,21 @@ let out_op0 (type v cont) : (v, cont) out_ State.op =
   let module M = struct
     type nonrec a = (v, cont) out_
 
-    let determinise ctx (Out (tag, name, (lazy cont))) =
-      Out (tag, name, lazy (PowState.determinise ctx cont))
+    let determinise ctx (tag, name, (lazy cont)) =
+      (tag, name, lazy (PowState.determinise ctx cont))
 
-    let merge ctx (Out (tag1, name1, cont1)) (Out (tag2, name2, cont2)) =
+    let merge ctx (tag1, name1, cont1) (tag2, name2, cont2) =
       assert (tag1 = tag2);
       DynChan.unify name1 name2;
-      Out
-        ( tag1,
-          name1,
-          lazy (PowState.merge ctx (Lazy.force cont1) (Lazy.force cont2)) )
+      ( tag1,
+        name1,
+        lazy (PowState.merge ctx (Lazy.force cont1) (Lazy.force cont2)) )
 
-    let force ctx (Out (_, name, cont)) =
+    let force ctx (_, name, cont) =
       ignore (DynChan.finalise name);
       PowState.force ctx (Lazy.force cont)
 
-    let to_string ctx (Out (_, _, cont)) =
+    let to_string ctx (_, _, cont) =
       (*FIXME:use label*)
       if Lazy.is_val cont then
         let cont = Lazy.force cont in
@@ -45,7 +41,7 @@ let select_op role label =
 let make_out role name s : _ LinState.t =
   let st =
     (role.make_obj |> Lin.map_gen)
-    @@ Lin.declare (Out ("_out" (* FIXME *), name, Lazy.from_val s))
+    @@ Lin.declare ("_out" (* FIXME *), name, Lazy.from_val s)
   in
   let op = LinState.gen_op (out_op role) in
   PowState.make op st
@@ -53,20 +49,20 @@ let make_out role name s : _ LinState.t =
 let make_select role label name s : _ LinState.t =
   let st =
     Lin.map_gen (fun t -> role.make_obj @@ label.make_obj t)
-    @@ Lin.declare (Out (label.method_name, name, Lazy.from_val s))
+    @@ Lin.declare (label.method_name, name, Lazy.from_val s)
   in
   let op = LinState.gen_op @@ select_op role label in
   PowState.make op st
 
 let select out =
-  let (Out (labname, name, cont)) = Lin.use out in
+  let labname, name, cont = Lin.use out in
   let tag = Btype.hash_variant labname in
   let cont = PowState.ensure_determinised (Lazy.force cont) in
   DynChan.send (DynChan.finalise name) tag;
   Lin.fresh cont
 
 let send out v =
-  let (Out (_, name, cont)) = Lin.use out in
+  let _, name, cont = Lin.use out in
   let cont = PowState.ensure_determinised (Lazy.force cont) in
   DynChan.send (DynChan.finalise name) v;
   Lin.fresh cont

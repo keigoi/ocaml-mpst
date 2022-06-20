054e032dff0ee560a4da4d5e716494a321346a5b
Keigo Imai <keigo.imai@gmail.com>
2022-06-20 15:46:57 +0900

    failing linearity tests

---
+++ tests/test_linearity.ml
@@ -0,0 +1,22 @@
+module Lin = Mpst.Lin
+open OUnit
+
+let test_unify () =
+  let unify = Lin.merge_lin (fun () () -> ()) in
+  let n1 = Lin.fresh @@ Lin.declare () in
+  let n2 = Lin.fresh @@ Lin.declare () in
+  let n3 = Lin.fresh @@ Lin.declare () in
+  let n12 = unify n1 n2 in
+  let n32 = unify n3 n2 in
+  Lin.use n1;
+  assert_raises Lin.InvalidEndpoint (fun () -> Lin.use n1);
+  assert_raises Lin.InvalidEndpoint (fun () -> Lin.use n2);
+  assert_raises Lin.InvalidEndpoint (fun () -> Lin.use n12);
+  assert_raises Lin.InvalidEndpoint (fun () -> Lin.use n32);
+  ()
+
+let suite = "Running mpst communication" >::: [ "test_unify" >:: test_unify ]
+
+let () =
+  let _results = run_test_tt_main suite in
+  ()
